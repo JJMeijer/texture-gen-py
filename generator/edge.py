16f691dcb16ddff9778f3aed72c6c02b4512e229
johan <6570485+tserest@users.noreply.github.com>
2020-05-23 13:52:58 +0200

    Texture Generator with edges

---
+++ generator/edge.py
@@ -0,0 +1,63 @@
+from PIL import ImageColor
+from random import randint
+
+class TextureEdgeGenerator():
+	def __init__(self, edge, texture):
+		self.texture = texture
+		self.edge_side = edge['side']
+		self.width = edge['width']
+		self.palette = edge['palette']
+
+		self.gen_prio_list()
+		self.set_edge()
+
+
+	def set_edge(self):        
+		texture_size = self.texture.size
+		edge_side = self.edge_side
+		width = self.width
+
+		if edge_side == 'top':
+			self.edge = {
+				'cols': list(range(texture_size[0])),
+				'rows': list(range(0, width))
+			}
+		elif edge_side == 'bottom':
+			self.edge = {
+				'cols': list(range(texture_size[0])),
+				'rows': list(range(texture_size[1] - width, texture_size[1]))
+			}
+		elif edge_side == 'left':
+			self.edge = {
+				'cols': list(range(0, width)),
+				'rows': list(range(texture_size[1]))
+			}
+		elif edge_side == 'right':
+			self.edge = {
+				'cols': list(range(texture_size[0] - width, texture_size[0])),
+				'rows': list(range(texture_size[1]))
+			}
+
+	
+	def gen_prio_list(self):
+		self.prio_list = []
+		for index, color in enumerate(self.palette):
+			self.prio_list += [index] * color['prio']
+	
+
+	def select_random_palette_color(self):
+		color_index = self.prio_list[randint(0, len(self.prio_list)-1)]
+		return self.palette[color_index]['hex']
+	
+
+	def gen_edge(self):
+		edge = self.edge
+		texture = self.texture
+
+		for row in edge['rows']:
+			for col in edge['cols']:
+				random_color = self.select_random_palette_color()
+				texture.putpixel((col, row), ImageColor.getrgb(random_color))
+		
+		return texture
+	
--- main.py
+++ main.py
@@ -1,25 +1,47 @@
-from generator import TextureGenerator
+from generator.texture import TextureGenerator
 
-palette = [
-	{
-		"hex": "#2b1d12",
-		"prio": 20
+texture_settings = {
+	"size": (20,20),
+	"core": {
+		"palette": [
+			{
+				"hex": "#2b1d12",
+				"prio": 20
+			},
+			{
+				"hex": "#855838",
+				"prio": 10
+			},
+			{
+				"hex": "#362417",
+				"prio": 80
+			},
+			{
+				"hex": "#33251a",
+				"prio": 10
+			}
+		]
 	},
-	{
-		"hex": "#855838",
-		"prio": 10
-	},
-	{
-		"hex": "#362417",
-		"prio": 80
-	},
-	{
-		"hex": "#33251a",
-		"prio": 10
-	}
-]
+	"edges": [
+		{
+			"side": "top",
+			"width": 2,
+			"palette": [
+				{
+					"hex": "#0b6623",
+					"prio": 90
+				},
+				{
+					"hex": "#109433",
+					"prio": 10
+				}
+			]
+		}
+	]
+}
 
-generator = TextureGenerator(palette, 10)
+
+generator = TextureGenerator(texture_settings)
 
 random_texture = generator.gen()
 
